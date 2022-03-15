f0d914e482b30bba9114da43b622d0280348ed16
Dillon <lamontdillon@gmail.com>
2022-03-15 02:55:14 -0400

    changes to github webhook code

--- hook/github-webhook-handler.php
+++ hook/github-webhook-handler.php
@@ -1,19 +1,23 @@
 <?php
+
 /**
  * GitHub webhook handler template.
  *
- * @see https://developer.github.com/webooks/
- * @author Miloslav Hůla (https://github.com/milo)
+ * @author "Miloslav Hůla <milo@github.com>"
+ * @see    https://developer.github.com/webooks/
  */
 
-# Additional to write a message string to a log file
-function LogMessage($message) {
-	global $logfile;
-	$logfile = 'UpdateDocs.log';
-	$time = time();
-	$date = date('Y-m-d H:i:s');
+/** 
+ * Additional to write a message string to a log file
+ **/ 
+function LogMessage($message)
+{
+    global $logfile;
+    $logfile = 'UpdateDocs.log';
+    $time = time();
+    $date = date('Y-m-d H:i:s');
 
-	file_put_contents($logfile, $date . "; " . $message . "\n", FILE_APPEND);
+    file_put_contents($logfile, $date . "; " . $message . "\n", FILE_APPEND);
 }
 
 
@@ -21,74 +25,71 @@ function LogMessage($message) {
 // Put this lne: SetEnv GITHUB_WEBHOOK_SECRET MY_SECRET
 $hookSecret = getenv('GITHUB_WEBHOOK_SECRET');
 
-set_error_handler(function($severity, $message, $file, $line) {
-	throw new \ErrorException($message, 0, $severity, $file, $line);
+set_error_handler(function ($severity, $message, $file, $line) {
+    throw new \ErrorException($message, 0, $severity, $file, $line);
 });
 
-set_exception_handler(function($e) {
-	header('HTTP/1.1 500 Internal Server Error');
-	echo "Error on line {$e->getLine()}: " . htmlSpecialChars($e->getMessage());
-	die();
+set_exception_handler(function ($e) {
+    header('HTTP/1.1 500 Internal Server Error');
+    echo "Error on line {$e->getLine()}: " . htmlSpecialChars($e->getMessage());
+    die();
 });
 
-$rawPost = NULL;
+$rawPost = null;
 
-if ($hookSecret !== NULL) {
-	if (!isset($_SERVER['HTTP_X_HUB_SIGNATURE'])) {
-		throw new \Exception("HTTP header 'X-Hub-Signature' is missing.");
-	} elseif (!extension_loaded('hash')) {
-		throw new \Exception("Missing 'hash' extension to check the secret code validity.");
-	}
+if ($hookSecret !== null) {
+    if (!isset($_SERVER['HTTP_X_HUB_SIGNATURE'])) {
+        throw new \Exception("HTTP header 'X-Hub-Signature' is missing.");
+    } elseif (!extension_loaded('hash')) {
+        throw new \Exception("Missing 'hash' extension to check the secret code validity.");
+    }
 
-	list($algo, $hash) = explode('=', $_SERVER['HTTP_X_HUB_SIGNATURE'], 2) + array('', '');
-	if(!in_array($algo, hash_algos(), TRUE)) {
-		throw new \Exception("Hash algorithm '$algo' is not supported.");
-	}
+    list($algo, $hash) = explode('=', $_SERVER['HTTP_X_HUB_SIGNATURE'], 2) + array('', '');
+    if (!in_array($algo, hash_algos(), true)) {
+        throw new \Exception("Hash algorithm '$algo' is not supported.");
+    }
 
-	$rawPost = file_get_contents('php://input');
-	if(!hash_equals($hash, hash_hmac($algo, $rawPost, $hookSecret))) {
-		throw new \Exception('Hook secret does not match.');
-	}
+    $rawPost = file_get_contents('php://input');
+    if (!hash_equals($hash, hash_hmac($algo, $rawPost, $hookSecret))) {
+        throw new \Exception('Hook secret does not match.');
+    }
 };
 
 if (!isset($_SERVER['CONTENT_TYPE'])) {
-	throw new \Exception("Missing HTTP 'Content-Type' header.");
+    throw new \Exception("Missing HTTP 'Content-Type' header.");
 } elseif (!isset($_SERVER['HTTP_X_GITHUB_EVENT'])) {
-	throw new \Exception("Missing HTTP 'X-Github-Event' header.");
+    throw new \Exception("Missing HTTP 'X-Github-Event' header.");
 }
 
 switch ($_SERVER['CONTENT_TYPE']) {
 case 'application/json':
-	$json = $rawPost ?: file_get_contents('php://input');
-	break;
+    $json = $rawPost ?: file_get_contents('php://input');
+    break;
 case 'application/x-www-form-urlencoded':
-	$json = $_POST['payload'];
-	break;
+    $json = $_POST['payload'];
+    break;
 default:
-	throw new \Exception("Unsuported content type: $_SERVER[CONTENT_TYPE]");
+    throw new \Exception("Unsuported content type: $_SERVER[CONTENT_TYPE]");
 }
 
 # Payload structure depends on trigger event
 # https://developser.github.com/v3/actieiy/events/types/
 $payload = json_decode($json);
 switch (strtolower($_SERVER['HTTP_X_GITHUB_EVENT'])) {
-	case 'ping':
-		echo 'pong';
-		break;
-	case 'push':
-		LogMessage("Push Event recieved from GitHub repository Document_site! Updating Book.");
-		# pull the orgin main from GitHub
-		# build the book based off of the new GitHub pull
-		$result = shell_exec('cd /var/www/com.zedicit/public && git pull origin main 2>&1 && mdbook build 2>&1');
-
-		LogMessage($result);
-		LogMessage("Update book command executed.");
-
-		break;
-	default:
-		header('HTTP/1.0 ror Not Found');
-		echo "Event:$_SERVER[HTTP_X_GITHUB_EVENT] Payload:\n";
-		LogMessage($payload); # For debug
-		die();
+case 'ping':
+    echo 'pong';
+    break;
+case 'push':
+    LogMessage("Push Event recieved from GitHub repository Document_site!Updating Book.");
+    # pull the orgin main from GitHub
+    # build the book based off of the new GitHub pull
+    $result = shell_exec('cd /var/www/com.zedicit/public/Documentation_site &&git pull origin main 2>&1 && mdbook build 2>&1');
+    LogMessage($result);
+    LogMessage("Update book command executed.");
+    break;
+default:
+    header('HTTP/1.0 ror Not Found');
+    echo "Event:$_SERVER[HTTP_X_GITHUB_EVENT] Payload:\n";
+    LogMessage($payload); # For debug
+    die();
 }
-?>
